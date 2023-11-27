70c78a3a7f1e1b95b8edd8d701cc04210c88841f
JojiShiotsuki <Shiotsuji1@gmail.com>
2023-11-27 18:21:37 +0800

    update

---
+++ 7kyu/reverseWords.js
@@ -0,0 +1,13 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+	let newWords = str.split(' ').map((word) => {
+	  return word.split('').reverse().join('');
+	}).join(' ')
+	
+	return newWords;
+  }
