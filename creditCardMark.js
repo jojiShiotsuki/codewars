4a152c0e4f50dd84caadddb28a13ff4776540481
JojiShiotsuki <Shiotsuji1@gmail.com>
2024-01-31 12:50:01 +0800

    replace rotten fruits

---
+++ creditCardMark.js
@@ -0,0 +1,40 @@
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples (input --> output):
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+// // "What was the name of your first pet?"
+// "Skippy" --> "##ippy"
+// "Nananananananananananananananana Batman!" --> "####################################man!"
+
+// return masked string
+function maskify(cc) {
+  
+	let ret = cc.split('')
+	
+	if(ret.length > 3){
+	  
+	  ret = ret.map((char, index) => {
+		
+		if(index < ret.length - 4){
+		  return '#'
+		}else {
+		  return char
+		}
+		
+	  }).join('')
+	  
+	}else {
+	  
+	  ret = cc
+	  
+	}
+	
+	return ret
+  }
+  
